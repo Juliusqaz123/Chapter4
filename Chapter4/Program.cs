b9b8653b74004baadb1ce0f54d9d99f6448994c0
Julius <juliusblusevicius20@gmail.com>
2019-10-01 14:52:32 +0300

    Creating a new directory

--- Chapter4/Program.cs
+++ Chapter4/Program.cs
@@ -11,31 +11,10 @@ namespace Chapter4
     {
         static void Main(string[] args)
         {
-            DriveInfo[] drivesInfo = DriveInfo.GetDrives();
+            var directory = Directory.CreateDirectory(@"C:\Temp\ProgrammingInCSharp\Directory");
 
-            foreach (DriveInfo driveInfo in drivesInfo)
-            {
-                Console.WriteLine("Drive {0}", driveInfo.Name);
-                Console.WriteLine("  File type: {0}", driveInfo.DriveType);
-
-                if (driveInfo.IsReady == true)
-                {
-                    Console.WriteLine("  Volume label: {0}", driveInfo.VolumeLabel);
-                    Console.WriteLine("  File system: {0}", driveInfo.DriveFormat);
-                    Console.WriteLine(
-                        "  Available space to current user:{0, 15} bytes",
-                        driveInfo.AvailableFreeSpace);
-
-                    Console.WriteLine(
-                        "  Total available space:          {0, 15} bytes",
-                        driveInfo.TotalFreeSpace);
-
-                    Console.WriteLine(
-                        "  Total size of drive:            {0, 15} bytes",
-                        driveInfo.TotalSize);
-                }
-                Console.ReadLine();
-            }
+            var directoryInfo = new DirectoryInfo(@"C:\Temp\ProgrammingInCSharp\DirectoryInfo");
+            directoryInfo.Create();
         }
     }
 }
