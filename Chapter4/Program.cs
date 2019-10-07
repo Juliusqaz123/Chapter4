3c2d9037423ba62c9bf3b4fcaf4655ec82cf3602
Julius <juliusblusevicius20@gmail.com>
2019-10-07 10:20:26 +0300

    Create and use a FileStream

--- Chapter4/Program.cs
+++ Chapter4/Program.cs
@@ -12,12 +12,14 @@ namespace Chapter4
     {
         static void Main(string[] args)
         {
-            string path = @"C:\temp\subdir\file.txt";
+            string path = @"c:\temp\test.dat";
 
-            Console.WriteLine(Path.GetDirectoryName(path)); // Displays C:\temp\subdir
-            Console.WriteLine(Path.GetExtension(path)); // Displays .txt
-            Console.WriteLine(Path.GetFileName(path));  // Displays file.txt
-            Console.WriteLine(Path.GetPathRoot(path)); // Displays C:\
+            using (FileStream fileStream = File.Create(path))
+            {
+                string myValue = "MyValue";
+                byte[] data = Encoding.UTF8.GetBytes(myValue);
+                fileStream.Write(data, 0, data.Length);
+            }
 
             Console.ReadLine();
         }
