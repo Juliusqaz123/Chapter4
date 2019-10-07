f8f71a6d0ca6f60242fd73c2922f374fe8d68a93
Julius <juliusblusevicius20@gmail.com>
2019-10-07 10:17:42 +0300

    Using other Path methods to parse a path

--- Chapter4/Program.cs
+++ Chapter4/Program.cs
@@ -12,10 +12,12 @@ namespace Chapter4
     {
         static void Main(string[] args)
         {
-            string folder = @"C:\temp";
-            string fileName = "test.dat";
+            string path = @"C:\temp\subdir\file.txt";
 
-            string fullPath = Path.Combine(folder, fileName);   // Results in C:\\temp\\test.dat
+            Console.WriteLine(Path.GetDirectoryName(path)); // Displays C:\temp\subdir
+            Console.WriteLine(Path.GetExtension(path)); // Displays .txt
+            Console.WriteLine(Path.GetFileName(path));  // Displays file.txt
+            Console.WriteLine(Path.GetPathRoot(path)); // Displays C:\
 
             Console.ReadLine();
         }
