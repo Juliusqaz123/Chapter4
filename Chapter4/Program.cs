607c5b0577c7a740cffd19eac34ee6f1897dc393
Julius <juliusblusevicius20@gmail.com>
2019-10-11 13:15:19 +0300

    Updating rows with ExecuteNonQuery

--- Chapter4/Program.cs
+++ Chapter4/Program.cs
@@ -185,5 +185,21 @@ namespace Chapter4
                 }
             }
         }
+
+        public async Task UpdateRows()
+        {
+            string connectionString = ConfigurationManager.
+                ConnectionStrings["ProgrammingInCSharpConnection"].ConnectionString;
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                SqlCommand command = new SqlCommand(
+                    "UPDATE People SET FirstName='John'",
+                    connection);
+
+                await connection.OpenAsync();
+                int numberOfUpdatedRows = await command.ExecuteNonQueryAsync();
+                Console.WriteLine("Updated {0} rows", numberOfUpdatedRows);
+            }
+        }
     }
 }
