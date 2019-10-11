42cdb5a6bc7ed199b592031388ee93c3f62e51cf
Julius <juliusblusevicius20@gmail.com>
2019-10-11 13:48:39 +0300

    Using Code First to map a class to the database

--- Chapter4/PeopleContext.cs
+++ Chapter4/PeopleContext.cs
@@ -1,14 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Chapter4
 {
-    public class Person
+    public class PeopleContext : DbContext
     {
-        public int Id { get; set; }
-        public string Name { get; set; }
+        public IDbSet<Person> People { get; set; }
     }
 }
--- Chapter4/Program.cs
+++ Chapter4/Program.cs
@@ -18,31 +18,17 @@ namespace Chapter4
     {
         static void Main(string[] args)
         {
-            string connectionString = ConfigurationManager.
-                ConnectionStrings["ProgrammingInCSharpConnection"].ConnectionString;
-
-            using (TransactionScope transactionScope = new TransactionScope())
+            using (PeopleContext ctx = new PeopleContext())
             {
-                using (SqlConnection connection = new SqlConnection(connectionString))
-                {
-                    connection.Open();
-
-                    SqlCommand command1 = new SqlCommand(
-                        "INSERT INTO People ([FirstName], [LastName], [MiddleInitial])" +
-                        "VALUES('John', 'Doe', null)",
-                        connection);
-                    SqlCommand command2 = new SqlCommand(
-                        "INSERT INTO People ([FirstName], [LastName], [MiddleInitial])" +
-                        "VALUES('Jane', 'Doe', null)",
-                        connection);
-
-                    command1.ExecuteNonQuery();
-                    command2.ExecuteNonQuery();
-                }
-                transactionScope.Complete();
+                ctx.People.Add(new Person() { Id = 1, Name = "John Doe" });
+                ctx.SaveChanges();
             }
 
-                Console.ReadLine();
+            using (PeopleContext ctx = new PeopleContext())
+            {
+                Person person = ctx.People.SingleOrDefault(p => p.Id == 1);
+                Console.WriteLine(person.Name);
+            }
         }
 
         private static void ListDirectories(DirectoryInfo directoryInfo,
