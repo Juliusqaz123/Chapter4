2a10c8ee12fe79b2e0461b492b1528e02100a785
Julius <juliusblusevicius20@gmail.com>
2019-10-11 13:06:13 +0300

    Executing a SQL select command

--- Chapter4/Program.cs
+++ Chapter4/Program.cs
@@ -111,5 +111,42 @@ namespace Chapter4
 
             await Task.WhenAll(microsoft, msdn, blogs);
         }
+
+        public async Task SelectDataFromTable()
+        {
+            string connectionString = ConfigurationManager.
+                ConnectionStrings["ProgrammingInCSharpConnection"].ConnectionString;
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                SqlCommand command = new SqlCommand("SELECT * FROM People", connection);
+                await connection.OpenAsync();
+
+                SqlDataReader dataReader = await command.ExecuteReaderAsync();
+
+                while (await dataReader.ReadAsync())
+                {
+                    string formatStringWithMiddleName = "Person ({0}) is named {1} {2} {3}";
+                    string formatStringWithoutMiddleName = "Person ({0}) is named {1} {3}";
+
+                    if ((dataReader["middlename"] == null))
+                    {
+                        Console.WriteLine(formatStringWithoutMiddleName,
+                            dataReader["id"],
+                            dataReader["firstname"],
+                            dataReader["lastname"]);
+                    }
+                    else
+                    {
+                        Console.WriteLine(formatStringWithMiddleName,
+                            dataReader["id"],
+                            dataReader["firstname"],
+                            dataReader["middlename"],
+                            dataReader["lastname"]);
+                    }
+                }
+                dataReader.Close();
+            }
+            
+        }
     }
 }
