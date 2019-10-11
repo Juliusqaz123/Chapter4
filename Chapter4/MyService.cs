d26b214b4a047cdd05560719c2ed77adbc1b7acf
Julius <juliusblusevicius20@gmail.com>
2019-10-11 13:51:02 +0300

    A simple WCF web service

---
+++ Chapter4/MyService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chapter4
+{
+    [ServiceContract]
+    public class MyService
+    {
+        [OperationContract]
+        public string DoWork(string left, string right)
+        {
+            return left + right;
+        }
+    }
+}
