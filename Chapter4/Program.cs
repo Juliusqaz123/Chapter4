713a2a6d3adb00acd42d4b5b0436870ac4297e59
Julius <juliusblusevicius20@gmail.com>
2019-10-24 16:50:44 +0300

    Implementing Where

--- Chapter4/Program.cs
+++ Chapter4/Program.cs
@@ -237,4 +237,20 @@ namespace Chapter4
     {
         public List<OrderLine> OrderLines { get; set; }
     }
+
+    public static class LinqExtensions
+    {
+        public static IEnumerable<TSource> Where<TSource>(
+            this IEnumerable<TSource> source,
+            Func<TSource, bool> predicate)
+        {
+            foreach (TSource item in source)
+            {
+                if (predicate(item))
+                {
+                    yield return item;
+                }
+            }
+        }
+    }
 }
