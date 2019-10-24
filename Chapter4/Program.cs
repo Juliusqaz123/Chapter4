9852adef80234ad42f2bf2d97876a564a7134505
Julius <juliusblusevicius20@gmail.com>
2019-10-24 15:26:07 +0300

    Creating an XML file with XmlWriter

--- Chapter4/Program.cs
+++ Chapter4/Program.cs
@@ -19,45 +19,28 @@ namespace Chapter4
     {
         static void Main(string[] args)
         {
-            string xml =
-                @"<?xml version=""1.0"" encoding=""utf-8"" ?>
-                    <people>
-                      <person firstName=""John"" lastName=""Doe"">
-                        <contactdetails>
-                          <emailaddress>john@unknown.com</emailaddress>
-                        </contactdetails>
-                      </person>
-                      <person firstName=""Jane"" lastName=""Doe"">
-                        <contactdetails>
-                          <emailaddress>jane@unknown.com</emailaddress>
-                          <phonenumber>001122334455</phonenumber>
-                        </contactdetails>
-                      </person>
-                    </people>";
+            StringWriter stream = new StringWriter();
 
-            using (StringReader stringReader = new StringReader(xml))
+            using (XmlWriter writer = XmlWriter.Create(
+                stream,
+                new XmlWriterSettings() { Indent = true }
+                ))
             {
-                using (XmlReader xmlReader = XmlReader.Create(stringReader,
-                    new XmlReaderSettings() { IgnoreWhitespace = true }))
-                {
-                    xmlReader.MoveToContent();
-                    xmlReader.ReadStartElement("people");
-
-                    string firstName = xmlReader.GetAttribute("firstName");
-                    string lastName = xmlReader.GetAttribute("lastName");
-
-                    Console.WriteLine("Person: {0} {1}", firstName, lastName);
-                    xmlReader.ReadStartElement("person");
-
-                    Console.WriteLine("contactdetails");
-
-                    xmlReader.ReadStartElement("contactdetails");
-                    string emailAddress = xmlReader.ReadString();
-
-                    Console.WriteLine("Email address:{0}", emailAddress);
-                    Console.ReadLine();
-                }
+                writer.WriteStartDocument();
+                writer.WriteStartElement("People");
+                writer.WriteStartElement("Person");
+                writer.WriteAttributeString("firstname", "John");
+                writer.WriteAttributeString("lastname", "Doe");
+                writer.WriteStartElement("ContactDetails");
+                writer.WriteElementString("EmailAddress", "john@unknown.com");
+                writer.WriteEndElement();
+                writer.WriteEndElement();
+                writer.Flush();
             }
+
+            Console.WriteLine(stream.ToString());
+
+            Console.ReadLine();
         }
 
         private static void ListDirectories(DirectoryInfo directoryInfo,
