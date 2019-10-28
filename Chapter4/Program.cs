1128cb79ffc5fb9c27cd396e69f6d20bf8dae98a
Julius <juliusblusevicius20@gmail.com>
2019-10-28 12:14:01 +0200

    Using the ISet<T> interface

--- Chapter4/Program.cs
+++ Chapter4/Program.cs
@@ -49,6 +49,20 @@ namespace Chapter4
             Console.ReadLine();
         }
 
+        public interface ISet<T> : ICollection<T>, IEnumerable<T>, IEnumerable
+        {
+            bool Add(T item);
+            void ExceptWith(IEnumerable<T> other);
+            void IntersectWith(IEnumerable<T> other);
+            bool IsProperSubsetOf(IEnumerable<T> other);
+            bool IsSubsetOf(IEnumerable<T> other);
+            bool IsSupersetOf(IEnumerable<T> other);
+            bool Overlaps(IEnumerable<T> other);
+            bool SetEquals(IEnumerable<T> other);
+            void UnionWith(IEnumerable<T> other);
+
+        }
+
         public interface IList<T> : ICollection<T>, IEnumerable<T>, IEnumerable
         {
             T this[int index] { get; set; }
