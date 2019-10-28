c95a3a8e07a66a7a5f86fc04321327103b76a069
Julius <juliusblusevicius20@gmail.com>
2019-10-28 11:41:00 +0200

    Implementing ISerializable

--- Chapter4/Program.cs
+++ Chapter4/Program.cs
@@ -17,6 +17,7 @@ using System.Xml.Linq;
 using System.Xml.Serialization;
 using System.Runtime.Serialization.Formatters.Binary;
 using System.Runtime.Serialization;
+using System.Security.Permissions;
 
 namespace Chapter4
 {
@@ -41,6 +42,32 @@ namespace Chapter4
                 Person dp = (Person)formatter.Deserialize(stream);
             }
             Console.ReadLine();
+        }
+
+        [Serializable]
+        public class PersonComplex : ISerializable
+        {
+            public int Id { get; set; }
+            public string Name { get; set; }
+            private bool isDirty = false;
+
+            public PersonComplex() { }
+            protected PersonComplex(SerializationInfo info, StreamingContext context)
+            {
+                Id = info.GetInt32("Value1");
+                Name = info.GetString("Value2");
+                isDirty = info.GetBoolean("Value3");
+            }
+
+            [System.Security.Permissions.SecurityPermission(SecurityAction.Demand,
+                                                            SerializationFormatter = true)]
+            public void GetObjectData(SerializationInfo info, StreamingContext context)
+            {
+                info.AddValue("Value1", Id);
+                info.AddValue("Value2", Name);
+                info.AddValue("Value3", isDirty);
+            }
+
         }
 
         private static Order CreateOrder()
